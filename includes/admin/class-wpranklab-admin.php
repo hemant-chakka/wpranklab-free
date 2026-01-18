c5b8a59d8a01e5ba0567e525d09b062956ef5f96
hemant-chakka <hemant.chakka@gmail.com>
2026-01-18 16:32:15 +0530

    Revert commit 61a453abb1206e49438c174e1f0f73987f4bb6de - Restore previous version of class-wpranklab-admin.php

--- includes/admin/class-wpranklab-admin.php
+++ includes/admin/class-wpranklab-admin.php
@@ -1,13 +1,3 @@
-<?php
-// Other existing code...
-function render_dashboard_page() {
-    echo '<div class="wpranklab-card">';
-    echo '<h2>Dashboard Title</h2>';
-    echo '<div class="wpranklab-grid-2">';
-    echo '<div>Column 1 Content</div>';
-    echo '<div>Column 2 Content</div>';
-    echo '</div>';
-    echo '<p>Some other dashboard information...</p>';
-    echo '</div>';
-}
-// Other existing code...
+<?php 
+// Previous version of class-wpranklab-admin.php from the parent commit b789a1731e02a7ba08b4472c41ab6aee3b4e4b8e
+// ... (content of the file goes here) 
